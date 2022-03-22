5a86e042a8d76d158b2e9d75c2b37d4fd7354b0e
Man Le <manneko1803@gmail.com>
2022-03-22 08:50:10 +0700

    add locust [ManLM1]

---
+++ locustfile.py
@@ -0,0 +1,13 @@
+import time
+from locust import HttpUser, task, TaskSet, between
+
+class MTaskSet(HttpUser):
+    wait_time = between(1, 2.5)
+
+    @task
+    def home(self):
+        self.client.get("/")
+
+    @task(3)
+    def predict(self):
+        self.client.post("/predict", json={"CHAS" :{"0" :0}, "RM" :{"0" :6.575}, "TAX" :{"0" :296.0}, "PTRATIO" :{"0" :15.3}, "B" :{"0" :396.9}, "LSTAT" :{"0" :4.98}})
